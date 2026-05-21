fb39d21f65d6100d4303ece2c83dee5330a4a949
Antony Jose <antonyjose777aj@gmail.com>
2026-05-22 01:57:04 +0530

    stripe public key hardcode fix .

--- travel_diary_frontend/src/components/PaymentModal.jsx
+++ travel_diary_frontend/src/components/PaymentModal.jsx
@@ -5,7 +5,7 @@ import { Elements, CardElement, useStripe, useElements } from '@stripe/react-str
 import api from '../services/api';
 
 // Put your public test key here
-const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
+const stripePromise = loadStripe("pk_test_51TYlajCZYVfTY1VlZx8q0jusnQvyd7rl2K6KNp3x8IVaz9GkvnYjgc8wStCLNzIIAqzxQnVqHByXPfTTDbZFuVsU000WkpffSF");
 
 const CheckoutForm = ({ booking, onSuccess }) => {
     const stripe = useStripe();
